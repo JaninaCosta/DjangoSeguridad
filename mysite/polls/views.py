b457d3f766fe32006be45033f3323ea5e23617f2
JaninaCosta <jany_costa23@hotmail.com>
2017-02-13 01:20:33 -0500

    proyecto

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello word")
+
+
+def search_form(request):
+    if 'start_date' in request.GET and 'end_date' in request.GET:
+        query = "SELECT * from myapp_mymodel WHERE myapp_mymodel.date "\
+                "BETWEEN '{} 00:00:00' AND '{} 23:59:59'".format(request.GET.get('start_date'),request.GET.get('end_date'))
+        model_items = MyModel.objects.raw(query)
+    else:
+        model_items = MyModel.objects.all()
+    return render(request, 'template.html', {'model_items': model_items})
